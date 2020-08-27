95e061738d7f390d41a45deba65ce78bdb410b78
Inklay <inklay@outlook.fr>
2020-08-27 23:16:23 +0200

    Updated server to Azure

--- src/version.hpp
+++ src/version.hpp
@@ -11,7 +11,7 @@ const std::wstring APP_VERSION = THIS_APP_VERSION;
 
 const std::wstring APP_URL_WEBSITE = L"https://github.com/Inklay/";
 
-const std::wstring APP_URL_BASE = L"https://inklay-glitch-webapp.glitch.me/SyncHelper/app/";
+const std::wstring APP_URL_BASE = L"https://inklaywebapp.azurewebsites.net/SyncHelper/app/";
 
 const std::wstring APP_URL_CHECK_VERSION = APP_URL_BASE + "check_version?v=" + APP_VERSION + "&hl=en";
 
