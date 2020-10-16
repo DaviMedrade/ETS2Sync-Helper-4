d5a6667a217144f6feb2b99644f3440c78fd1582
Inklay <inklay@outlook.fr>
2020-10-16 19:49:21 +0200

    Updated version and server URL

--- src/version.hpp
+++ src/version.hpp
@@ -2,8 +2,7 @@
 
 #include <string>
 
-// put this here so it's easier to bump
-#define THIS_APP_VERSION L"5.7.0 PR2"
+#define THIS_APP_VERSION L"5.7.0"
 
 const std::wstring APP_NAME = L"ETS2-ATS-Sync-Helper";
 const std::wstring APP_DISPLAY_NAME = L"ETS2&ATS Sync Helper";
@@ -11,8 +10,8 @@ const std::wstring APP_VERSION = THIS_APP_VERSION;
 
 const std::wstring APP_URL_WEBSITE = L"https://github.com/Inklay/";
 
-const std::wstring APP_URL_BASE = L"https://inklaywebapp.azurewebsites.net/SyncHelper/app/";
+const std::wstring APP_URL_BASE = L"http://webservices-env.eba-nh6hq243.us-east-1.elasticbeanstalk.com/SyncHelper/app/";
 
-const std::wstring APP_URL_CHECK_VERSION = APP_URL_BASE + "check_version?v=" + APP_VERSION + "&hl=en";
+const std::wstring APP_URL_CHECK_VERSION = APP_URL_BASE + "check_version?v=" + APP_VERSION;
 
-const std::wstring APP_URL_SYNC = APP_URL_BASE + "sync?v=" + APP_VERSION + "&hl=en";
+const std::wstring APP_URL_SYNC = APP_URL_BASE + "sync?v=" + APP_VERSION;
