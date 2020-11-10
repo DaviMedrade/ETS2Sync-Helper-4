6f2096ae7e6016982fc66f9a56916bddee935979
Inklay <inklay@outlook.fr>
2020-11-10 15:35:28 +0100

    Updated app version for stable release

--- src/version.hpp
+++ src/version.hpp
@@ -2,7 +2,7 @@
 
 #include <string>
 
-#define THIS_APP_VERSION L"5.8.0 PR1"
+#define THIS_APP_VERSION L"5.8.0"
 
 const std::wstring APP_NAME = L"ETS2-ATS-Sync-Helper";
 const std::wstring APP_DISPLAY_NAME = L"ETS2&ATS Sync Helper";
